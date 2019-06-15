2a7749bf275ca6ff63625311039fc9c71d59aca2
PWithanage <psathsaranee@gmail.com>
2019-06-15 13:01:09 +0530

    first commit

---
+++ src/main/java/One.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by ASUS on 6/15/2019.
+ */
+public class One {
+
+
+    public static void main(String[] args) {
+        Two two = new Two();
+        Scanner scanner = new Scanner(System.in) ;
+
+        System.out.print("Enter num 1 : ");
+
+        Integer input1 = scanner.nextInt();  // Read user input
+
+        System.out.print("Enter num 2 : ");
+
+        Integer input2 = scanner.nextInt();  // Read user input
+
+        two.add(input1,input2);
+
+    }
+}
---
+++ src/main/java/Two.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ASUS on 6/15/2019.
+ */
+public class Two {
+
+    public static void add(int i, int j){
+        int sum = i+j;
+        System.out.println("Sum = "+ sum);
+    }
+}
